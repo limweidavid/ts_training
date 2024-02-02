5f58ea433785db91c552dd004c2d6c001e606221
Wei Lim <weilim12@atletx7-reg155.amd.com>
2024-02-01 21:19:57 -0500

    add python import

---
+++ training/python/import/a.py
@@ -0,0 +1,13 @@
+def printmain():
+    print("__name__ is set to: {}".format(__name__))
+
+def runtesta():
+    print("run a lot of test, but only related to a")
+
+def main():
+    printmain()
+    runtesta()
+
+
+if __name__ == "__main__":
+    main()
